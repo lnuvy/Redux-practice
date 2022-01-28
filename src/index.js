57799bf26d7c818519a58a7315b004d733cef873
lnuvy <hangeulgod@naver.com>
2022-01-28 21:32:48 +0900

    practice 기본 세팅

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const number = document.querySelector("span");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let count = 0;
+
+number.innerText = count;
+
+const handleAdd = () => {
+  count = count + 1;
+};
+
+const handleMinus = () => {
+  count += 1;
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
