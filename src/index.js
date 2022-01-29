f5f004b65e83eaf3e6e5758e19136215f94fd43e
lnuvy <hangeulgod@naver.com>
2022-01-30 08:52:25 +0900

    Redux 를 사용할때 state 를 절대 mutation하면 안됨

--- src/index.js
+++ src/index.js
@@ -4,21 +4,34 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createTodo = (Todo) => {
-  const li = document.createElement("li");
-  li.innerText = Todo;
-  ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DLELTE_TODO = "DLELTE_TODO";
+
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text }];
+    case DLELTE_TODO:
+      return [];
+    default:
+      return state;
+  }
 };
+
+const store = createStore(reducer);
+
+// const createTodo = (Todo) => {
+//   const li = document.createElement("li");
+//   li.innerText = Todo;
+//   ul.appendChild(li);
+// };
 
 const onSubmit = (e) => {
   e.preventDefault();
   const Todo = input.value;
-  createTodo(Todo);
   input.value = "";
+  store.dispatch({ type: ADD_TODO, text: Todo });
 };
 
 form.addEventListener("submit", onSubmit);
-
-const reducer = (state = [], action) => {};
-
-const store = createStore();
