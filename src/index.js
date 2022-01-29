7493d025ecdfbb7b75e2d8feb3cb318dbc1f8753
lnuvy <hangeulgod@naver.com>
2022-01-30 08:43:38 +0900

    redux Todo리스트 데이터 관리하기 시작 (기본세팅)

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-// Reducer 데이터가 수정되는 유일한 함수
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-      break;
-    case MINUS:
-      return count - 1;
-      break;
-    default:
-      return count;
-  }
+const createTodo = (Todo) => {
+  const li = document.createElement("li");
+  li.innerText = Todo;
+  ul.appendChild(li);
 };
 
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
+const onSubmit = (e) => {
+  e.preventDefault();
+  const Todo = input.value;
+  createTodo(Todo);
+  input.value = "";
 };
 
-countStore.subscribe(onChange);
+form.addEventListener("submit", onSubmit);
 
-add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
-minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
+const reducer = (state = [], action) => {};
+
+const store = createStore();
