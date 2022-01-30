735f88c4f720bb87d6c970e37cf1b9d4d2140598
lnuvy <hangeulgod@naver.com>
2022-01-30 09:26:54 +0900

    HTML로부터 받아오는 id값은 String이기때문에 !== 를하면 삭제가 안된다

--- src/index.js
+++ src/index.js
@@ -5,15 +5,30 @@ const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
 const ADD_TODO = "ADD_TODO";
-const DLELTE_TODO = "DLELTE_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const addTodo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteTodo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
 
 const reducer = (state = [], action) => {
-  console.log(action);
+  console.log(state, action);
   switch (action.type) {
     case ADD_TODO:
       return [...state, { text: action.text, id: Date.now() }];
-    case DLELTE_TODO:
-      return [];
+    case DELETE_TODO:
+      console.log(action.id);
+      return state.filter((todo) => todo.id != action.id);
     default:
       return state;
   }
@@ -26,23 +41,33 @@ const paintTodos = () => {
   ul.innerHTML = "";
   toDos.forEach((toDo) => {
     const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "삭제";
+    btn.addEventListener("click", dispatchDeleteTodo);
     li.id = toDo.id;
     li.innerText = toDo.text;
     ul.appendChild(li);
+    li.appendChild(btn);
   });
 };
 
 store.subscribe(paintTodos);
 
-const addTodo = (text) => {
-  store.dispatch({ type: ADD_TODO, text });
+const dispatchAddTodo = (text) => {
+  store.dispatch(addTodo(text));
+};
+const dispatchDeleteTodo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteTodo(id));
 };
 
 const onSubmit = (e) => {
   e.preventDefault();
   const Todo = input.value;
   input.value = "";
-  addTodo(Todo);
+  if (Todo !== "") {
+    dispatchAddTodo(Todo);
+  }
 };
 
 form.addEventListener("submit", onSubmit);
