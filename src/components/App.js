98625e944eee92a824a15e4b8c0a40f989b25dd3
lnuvy <hangeulgod@naver.com>
2022-01-31 23:15:15 +0900

    Home은 나오는데 Detail이 안나오네

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../route/Home";
+import Detail from "../route/Detail";
 
 function App() {
-  return "App";
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
